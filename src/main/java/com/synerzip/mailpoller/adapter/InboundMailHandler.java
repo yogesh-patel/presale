31da2bc0023e891cdd2a92e02c8c4b53879b7133
yogesh-patel <yogesh.patel@synerzip.com>
2015-02-17 13:57:44 +0530

    LoginPage

--- src/main/java/com/synerzip/mailpoller/adapter/InboundMailHandler.java
+++ src/main/java/com/synerzip/mailpoller/adapter/InboundMailHandler.java
@@ -1,5 +1,11 @@
 package com.synerzip.mailpoller.adapter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.mail.Address;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
@@ -14,12 +20,107 @@ public class InboundMailHandler {
 		if (payload instanceof MimeMessage) {
 			MimeMessage mailMessage = (MimeMessage) payload;
 			try {
-				System.out.println(mailMessage.getSubject());
-				System.out.println(mailMessage.getSender());
-				System.out.println(mailMessage.getContentType());
-				System.out.println(mailMessage.getFrom()[0]);
-				System.out.println(mailMessage.getReplyTo()[0]);
-				System.out.println(mailMessage.getAllRecipients());
+				StringBuffer contentStrBuffer = null;
+				String subject = mailMessage.getSubject();
+				String emailFrom = mailMessage.getFrom()[0].toString();
+				Address[] recipients = mailMessage.getAllRecipients();
+				StringBuffer recipientStrBuffer = new StringBuffer();
+				String companyURL = "";
+				if (recipients != null) {
+					for (Address address : recipients) {
+						recipientStrBuffer.append(address.toString());
+						recipientStrBuffer.append("|");
+					}
+				}
+				Object content = mailMessage.getContent();
+				if (mailMessage.getContentType().contains("multipart")) {
+					if (content instanceof Multipart) {
+						Multipart multiPart = (Multipart) content;
+						for (int i = 0; i < multiPart.getCount(); i++) {
+							MimeBodyPart part = (MimeBodyPart) multiPart
+									.getBodyPart(i);
+							if (part.getContentType().contains("text/plain")) {
+								contentStrBuffer = new StringBuffer();
+								BufferedReader br = new BufferedReader(
+										new InputStreamReader(
+												part.getInputStream()));
+								String line = null;
+								while ((line = br.readLine()) != null) {
+									if ((line.startsWith("On") && line
+											.endsWith(">"))
+											|| (line.contains("Forwarded message"))) {
+										break;
+									}
+									contentStrBuffer.append(line);
+								}
+							} else if (part.getContentType().contains(
+									"text/html")) {
+								contentStrBuffer = new StringBuffer();
+								BufferedReader br = new BufferedReader(
+										new InputStreamReader(
+												part.getInputStream()));
+								String line = null;
+								while ((line = br.readLine()) != null) {
+									if (line.contains("href")) {
+
+										for (int index = line
+												.indexOf("href=\"") + 6; index < line
+												.length(); index++) {
+											companyURL += line.charAt(index);
+											if (companyURL.lastIndexOf("com") != -1
+													|| companyURL
+															.lastIndexOf("net") != -1
+													|| companyURL
+															.lastIndexOf("org") != -1) {
+												break;
+											}
+										}
+									}
+								}
+							}
+						}
+
+					}
+				}
+				if(subject.contains("Re:")){
+					//There should be entry for this prospects in DB, 
+				}
+				subject = subject.replaceAll("Re:", "").trim();
+				subject = subject.replaceAll("Fwd:", "").trim();
+
+				// Remove word like Oppty , opportunity , prospect
+				subject = subject.replaceAll("Oppty", "").trim();
+				subject = subject.replaceAll("opportunity", "").trim();
+				subject = subject.replaceAll("Opportunity", "").trim();
+				subject = subject.replaceAll("prospect", "").trim();
+				subject = subject.replaceAll("Prospect", "").trim();
+
+				// If subject contains "-" or ":"
+				String techStack = "";
+				String prospectName = subject;
+				if (subject.contains("-")) {
+					String[] moreDesc = subject.split("-");
+					if (moreDesc.length > 0) {
+						prospectName = moreDesc[0].trim();
+					}
+					if (moreDesc.length > 1) {
+						techStack = moreDesc[1].trim();
+					}
+
+				} else if (subject.contains(":")) {
+					String[] moreDesc = subject.split(":");
+					if (moreDesc.length > 0) {
+						prospectName = moreDesc[0].trim();
+					}
+					if (moreDesc.length > 1) {
+						techStack = moreDesc[1].trim();
+					}
+
+				}
+
+				System.out.println(prospectName + "\n" + companyURL + "\n"
+						+ techStack + "\n" + emailFrom + "\n"
+						+ recipientStrBuffer + "\n" + contentStrBuffer);
 			} catch (Throwable t) {
 
 			}
--- webapp/index.jsp
+++ webapp/index.jsp
@@ -1,5 +1,108 @@
 <html>
-<body>
-<h2>Hello World!</h2>
+<head>
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"
+	media="screen,projection" />
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+</head>
+<body class="blue-grey lighten-5">
+	<nav class="blue-grey darken-2">
+		<div class="nav-wrapper">
+			<div class="col s12">
+				<a href="#" class="brand-logo"><img src="images/logo-small.png" />PreSale<span
+					style="color: #ee6e73" class="badge hide-on-small-only">Beta</span></a>
+				<a href="#" data-activates="mobile-options" class="button-collapse"><i
+					class="mdi-navigation-menu"></i></a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="#">Dashboard</a></li>
+					<li><a href="#">Trend</a></li>
+					<li><a href="#">JavaScript</a></li>
+				</ul>
+				<ul class="side-nav" id="mobile-options">
+					<li><a href="#">Dashboard</a></li>
+					<li><a href="#">Trend</a></li>
+					<li><a href="#">JavaScript</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="row">
+		<div class="col m8">
+			<p class="flow-text hide-on-small-only">One common flaw we've seen in many
+				frameworks is a lack of support for truly responsive text. While
+				elements on the page resize fluidly, text still resizes on a fixed
+				basis. To ameliorate this problem, for text heavy pages, we've
+				created a class that fluidly scales text size and line-height to
+				optimize readability for the user. Line length stays between 45-80
+				characters and line height scales to be larger on smaller screens.
+
+				To see Flow Text in action, slowly resize your browser and watch the
+				size of this text body change! Use the button above to toggle off/on
+				flow-text to see the difference!</p>
+		</div>
+		<div class="col m4 hide-on-small-only">
+			<div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+					<span class="card-title">Login</span>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="username" type="text" class="validate"> <label
+								for="username">Username</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="password" type="password" class="validate"> <label
+								for="password">Password</label>
+						</div>
+					</div>
+				</div>
+				<div class="card-action">
+					<button class="btn waves-effect waves-light " type="submit"
+						name="action">
+						Submit <i class="mdi-content-send right"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 hide-on-med-and-up">
+			<div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+					<span class="card-title">Login</span>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="username" type="text" class="validate"> <label
+								for="username">Username</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="password" type="password" class="validate"> <label
+								for="password">Password</label>
+						</div>
+					</div>
+				</div>
+				<div class="card-action">
+					<button class="btn waves-effect waves-light" type="submit"
+						name="action">
+						Submit <i class="mdi-content-send right"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript"
+		src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$(".button-collapse").sideNav();
+		});
+	</script>
 </body>
 </html>
